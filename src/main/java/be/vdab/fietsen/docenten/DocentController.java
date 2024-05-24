46f6aa871dedfd2e121f65d58f573efa3b0f0c11
semihkenan <altuntas.semih34@gmail.com>
2024-05-24 09:33:51 +0200

    2. dag JPA-Hibernate

---
+++ src/main/java/be/vdab/fietsen/docenten/DocentController.java
@@ -0,0 +1,39 @@
+package be.vdab.fietsen.docenten;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.print.Doc;
+import java.util.List;
+
+@RestController
+@RequestMapping("docenten")
+public class DocentController {
+    private final DocentService docentService;
+
+    public DocentController(DocentService docentService) {
+        this.docentService = docentService;
+    }
+
+    @GetMapping("aantal")
+    long findAantal() {
+        return docentService.findAantal();
+    }
+
+    @GetMapping
+    List<Docent> findAll() {
+        return docentService.findAll();
+    }
+
+    @GetMapping("{id}")
+    Docent findById(@PathVariable long id) {
+        return docentService.findById(id).orElseThrow(DocentNietGevondenException::new);
+    }
+
+    @GetMapping("{id}/bestaat")
+    boolean bestaatById(@PathVariable long id) {
+        return docentService.existsById(id);
+    }
+}
